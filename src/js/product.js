80cbc175081f4666ff5028ded6b9e3a75350b66d
florenciataravella <bunibliss@gmail.com>
2026-03-06 10:42:52 -0300

    Fixed cart broken

--- src/js/product.js
+++ src/js/product.js
@@ -1,12 +1,15 @@
-import { setLocalStorage } from './utils.mjs';
+import { getLocalStorage, setLocalStorage } from './utils.mjs';
 import ProductData from './ProductData.mjs';
 
 const dataSource = new ProductData('tents');
 
 function addProductToCart(product) {
-  setLocalStorage('so-cart', product);
+  
+  const cartItems = getLocalStorage("so-cart") || [];
+  cartItems.push(product)
+  setLocalStorage("so-cart", cartItems) ;
+
 }
-// add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
   addProductToCart(product);
@@ -14,5 +17,5 @@ async function addToCartHandler(e) {
 
 // add listener to Add to Cart button
 document
-  .getElementById('addToCart')
+  .getElementById("addToCart")
   .addEventListener('click', addToCartHandler);
