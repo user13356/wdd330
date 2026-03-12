d9bb2d2a80a95ae7569c22440cc8f1da1a8fbb71
VinoliaMoekwa <nmoekwa@byupathway.edu>
2026-03-12 13:19:05 +0200

    . fixed product.js

--- src/js/product.js
+++ src/js/product.js
@@ -4,16 +4,18 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
+  // get cart array from localStorage, or empty array if null
   const cartItems = getLocalStorage("so-cart") || [];
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
+  cartItems.push(product); // add new product
+  setLocalStorage("so-cart", cartItems); // save updated cart
 }
+
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
   addProductToCart(product);
 }
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+// Add event listeners to all add-to-cart buttons
+document.querySelectorAll(".add-to-cart").forEach((btn) => {
+  btn.addEventListener("click", addToCartHandler);
+});
